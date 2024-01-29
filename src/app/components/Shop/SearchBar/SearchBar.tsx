3546dc8214906cd759b960ac1b97ae789a3d10c8
jduncan017 <emailjoshduncan@gmail.com>
2024-01-29 01:24:13 -0700

    add to admin panel, add next shap (npm)

--- src/app/components/Shop/SearchBar/SearchBar.tsx
+++ src/app/components/Shop/SearchBar/SearchBar.tsx
@@ -73,7 +73,7 @@ const SearchBar = () => {
     const sortedWoodTypes = Array.from(woodTypesSet).sort();
 
     setWoodTypes(sortedWoodTypes);
-  }, [imageCards]);
+  }, []);
 
   return (
     <form
