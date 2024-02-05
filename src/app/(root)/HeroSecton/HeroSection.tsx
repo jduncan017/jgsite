1ec12d5e22b0c3acb53b9156997f828576a060fd
jduncan017 <emailjoshduncan@gmail.com>
2024-02-05 14:37:40 -0700

    update hero-section ui

--- src/app/(root)/HeroSecton/HeroSection.tsx
+++ src/app/(root)/HeroSecton/HeroSection.tsx
@@ -20,6 +20,14 @@ const HeroSection = () => {
           placeholder="blur"
           priority
         />
+        <Image
+          src={woodAccent}
+          alt="header accent"
+          className="wood-accent_top global__wood-accent global__wood-accent_reversed"
+          width={1700}
+          height={53}
+          placeholder="blur"
+        />
         <div className="banner__textbox global__box-shadow">
           <Image
             className="banner__logo"
@@ -28,14 +36,14 @@ const HeroSection = () => {
             width={380}
             height={121}
           />
-          <h2
+          <p
             className={`banner__textbox-text global__text-shadow ${inter.className}`}
           >
             Quality custom woodworking designs that blend precision and quality,
             crafted from a variety of hand-selected woods to suit your needs.
             From live edge and luxurious resin furniture, we aim to create
             standout pieces that merge nature and art.
-          </h2>
+          </p>
           <Link href={"/our-story"}>
             <button className="banner__button global__button">
               Learn More
@@ -46,7 +54,7 @@ const HeroSection = () => {
       <Image
         src={woodAccent}
         alt="header accent"
-        className="global__wood-accent global__wood-accent_reversed"
+        className="wood-accent_bottom global__wood-accent global__wood-accent_reversed"
         width={1700}
         height={53}
         placeholder="blur"
