3b3b7443ae24500ccef00ea19196533f054483a3
jduncan017 <emailjoshduncan@gmail.com>
2024-02-05 13:15:45 -0700

    fix hero-section

--- src/app/(root)/HeroSecton/HeroSection.tsx
+++ src/app/(root)/HeroSecton/HeroSection.tsx
@@ -10,15 +10,39 @@ import Link from "next/link";
 const HeroSection = () => {
   return (
     <header className="banner">
-      <Image
-        className="banner__image"
-        src={bannerImage}
-        alt="banner image"
-        width={1700}
-        height={715}
-        placeholder="blur"
-        priority
-      />
+      <div className="banner__image-container">
+        <Image
+          className="banner__image"
+          src={bannerImage}
+          alt="banner image"
+          width={1700}
+          height={715}
+          placeholder="blur"
+          priority
+        />
+        <div className="banner__textbox global__box-shadow">
+          <Image
+            className="banner__logo"
+            src={fullLogoWhite}
+            alt="JG logo"
+            width={380}
+            height={121}
+          />
+          <h2
+            className={`banner__textbox-text global__text-shadow ${inter.className}`}
+          >
+            Quality custom woodworking designs that blend precision and quality,
+            crafted from a variety of hand-selected woods to suit your needs.
+            From live edge and luxurious resin furniture, we aim to create
+            standout pieces that merge nature and art.
+          </h2>
+          <Link href={"/our-story"}>
+            <button className="banner__button global__button">
+              Learn More
+            </button>
+          </Link>
+        </div>
+      </div>
       <Image
         src={woodAccent}
         alt="header accent"
@@ -27,26 +51,6 @@ const HeroSection = () => {
         height={53}
         placeholder="blur"
       />
-      <div className="banner__textbox global__box-shadow">
-        <Image
-          className="banner__logo"
-          src={fullLogoWhite}
-          alt="JG logo"
-          width={380}
-          height={121}
-        />
-        <h2
-          className={`banner__textbox-text global__text-shadow ${inter.className}`}
-        >
-          Quality custom woodworking designs that blend precision and quality,
-          crafted from a variety of hand-selected woods to suit your needs. From
-          live edge and luxurious resin furniture, we aim to create standout
-          pieces that merge nature and art.
-        </h2>
-        <Link href={"/our-story"}>
-          <button className="banner__button global__button">Learn More</button>
-        </Link>
-      </div>
     </header>
   );
 };
