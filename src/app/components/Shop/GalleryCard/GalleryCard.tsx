fc8383a6e09ee96387d4b4ede496f738a2f8b6a9
jduncan017 <emailjoshduncan@gmail.com>
2024-02-07 16:11:28 -0700

    fix price on gallerycard

--- src/app/components/Shop/GalleryCard/GalleryCard.tsx
+++ src/app/components/Shop/GalleryCard/GalleryCard.tsx
@@ -30,6 +30,7 @@ const GalleryCard: React.FC<GalleryCardProps> = ({
             alt={title}
             width={500}
             height={500}
+            placeholder="empty"
           ></Image>
         </ImageLoadingWrapper>
         <h3 className={`gallery-card__image-title ${cardo.className}`}>
@@ -37,7 +38,9 @@ const GalleryCard: React.FC<GalleryCardProps> = ({
         </h3>
       </div>
       <div className="gallery-card__info-container">
-        <p className="gallery-card__price">{formatCurrency(price)}</p>
+        <p className="gallery-card__price">
+          {qty > 0 ? formatCurrency(price) : ""}
+        </p>
         <button
           className={`gallery-card__button global__button ${cardo.className}`}
           type="button"
